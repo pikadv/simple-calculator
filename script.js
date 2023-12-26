ce62b57b3672f8c2f99fc82d361bb6ec6c8995f5
Pika <102599027+pikadv@users.noreply.github.com>
2023-12-26 21:02:34 +0600

    fix main display size

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const display = document.querySelector(".display")
 
 function appendValue(value) {
-  display.value += value
+  display.textContent += value
 }
 
 function clearDisplay() {
-  display.value = ""
+  display.textContent = ""
 }
 
 function calculate() {
-  if (!display.value) return
-  const result = eval(display.value)
-  display.value = result
+  if (!display.textContent) return
+  const result = eval(display.textContent)
+  display.textContent = result
 }
 
 function goBack() {
-  display.value = display.value.slice(0, -1)
+  display.textContent = display.textContent.slice(0, -1)
 }
